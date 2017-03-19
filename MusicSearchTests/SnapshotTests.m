b2937757d7ad9de072db30cd6eba497ff2a26780
Jordi Serra i Font <jordi.serra.font@gmail.com>
2017-03-19 13:17:20 +0100

    snapshot of detailVC

--- MusicSearchTests/SnapshotTests.m
+++ MusicSearchTests/SnapshotTests.m
@@ -47,7 +47,8 @@
     TrackDetail_ViewModel *viewModel = [[TrackDetail_ViewModel alloc] initWithName:@"Jack Johnson - Better Together" andAudioPath:@"http://a25.phobos.apple.com/us/r30/Music6/v4/13/22/67/1322678b-e40d-fb4d-8d9b-3268fe03b000/mzaf_8818596367816221008.plus.aac.p.m4a" andImagePath:@"http://is3.mzstatic.com/image/thumb/Music2/v4/a2/66/32/a2663205-663c-8301-eec7-57937c2d0878/source/100x100bb.jpg"];
     
     TrackDetailViewController *vc = [[TrackDetailViewController alloc] initWithViewModel:viewModel];
-    [self debugViewController:vc];
+    [self presentViewController:vc];
+    FBSnapshotVerifyView(self.window, @"TrackDetail");
 }
 
 @end
