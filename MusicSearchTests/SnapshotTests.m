efa25db66b454d889a690fa2a3cffa9e86428df0
Jordi Serra i Font <jordi.serra.font@gmail.com>
2017-03-19 20:53:32 +0100

    record mode off

--- MusicSearchTests/SnapshotTests.m
+++ MusicSearchTests/SnapshotTests.m
@@ -24,7 +24,7 @@
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
     
-    self.recordMode = YES;
+    self.recordMode = NO;
 }
 
 - (void)tearDown {
